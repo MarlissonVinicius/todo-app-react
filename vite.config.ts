0513b6a2fdc713a571d796688cacb7360c5d0fbc
marlisson <marlissonvinicius13@gmail.com>
2026-04-19 14:57:45 -0300

    configuração para deploy

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react-swc'
 import tailwindcss from '@tailwindcss/vite'
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/",
   plugins: [
     tailwindcss(),react()],
 })
